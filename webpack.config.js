eb09b0ff8504f353e95ea18bde55815c0a99ae3a
zjason25 <90489418+zjason25@users.noreply.github.com>
2024-07-16 15:19:13 -0700

    Add css loader  and webpack watch script

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
