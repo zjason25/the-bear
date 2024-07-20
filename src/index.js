da8e0ad6f04b6a84e3c7de2d3ccd86c6ddf8d706
zjason25 <90489418+zjason25@users.noreply.github.com>
2024-07-20 14:59:03 -0700

    Add function to open and close reservation form

--- src/index.js
+++ src/index.js
@@ -180,8 +180,42 @@ function addMenuContent() {
   content.appendChild(menu_container);
 }
 
+const openModalButtons = document.querySelectorAll("[data-modal-target]");
+const closeModalButtons = document.querySelectorAll("[data-close-button]");
+const overlay = document.getElementById('overlay');
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.getElementById('reservation-form');
+    closeModal(modal);
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) {
+    return;
+  }
+  modal.classList.add('active');
+  overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+  if (modal == null) {
+    return;
+  }
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+}
+
 function startUp() {
   addHomeContent();
 }
 
-// startUp();
+startUp();
